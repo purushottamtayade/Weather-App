05a2b17fec86795697477b636700d45df36ac304
Student P_06 <studentp_06@student-p-06.local>
2016-08-04 16:46:05 +0530

    Initial Commit

---
+++ PTWeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PTWeatherApp
+//
+//  Created by Student P_06 on 04/08/16.
+//  Copyright © 2016 Purushottam Tayade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PTWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PTWeatherApp
+//
+//  Created by Student P_06 on 04/08/16.
+//  Copyright © 2016 Purushottam Tayade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
